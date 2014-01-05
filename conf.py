51348df83f99c4a451e5cbb57f93a3a1c7dba5b3
Kouhei Maeda <mkouhei@palmtb.net>
2014-01-05 15:51:12 +0900

    change theme modern5 to dark
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- conf.py
+++ conf.py
@@ -33,7 +33,7 @@ disqus_shortname = 'mkouhei'
 html_favicon = 'tinkerer.ico'           
 
 # Pick another Tinkerer theme or use your own
-html_theme = "modern5"
+html_theme = "dark"
 
 # Theme-specific options, see docs
 html_theme_options = { }                                  
